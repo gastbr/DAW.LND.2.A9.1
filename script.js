7ed83ad26e8d2d68fa07ca4257754e4012d58457
Alumgs <alumgs@majadamarcial.es>
2024-02-28 21:57:18 +0000

    commit

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ a = Number(prompt("Introduce valor de A"))
 b = Number(prompt("Introduce valor de B"))
 c = Number(prompt("Introduce valor de C"))
 
-console.log(a+b+c)
-
+console.log("Este es el valor de a: "+ a)
+console.log("Este es el valor de b: "+ b)
+console.log("Este es el valor de c: "+ c)
+console.log("Resultado de a + b + c = " (a + b + c))
