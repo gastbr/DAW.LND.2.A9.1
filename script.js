70add9da8237f1be75d95e0466678677af8f20c9
Alumgs <alumgs@majadamarcial.es>
2024-02-28 22:54:14 +0000

    yes

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-a = Number(prompt("Introduce valor de A"))
-b = Number(prompt("Introduce valor de B"))
-c = Number(prompt("Introduce valor de C"))
+a = 0
+b = 0
+c = 0
 
-console.log("Este es el valor de a: "+ a)
-console.log("Este es el valor de b: "+ b)
-console.log("Este es el valor de c: "+ c)
-console.log("Resultado de a + b + c = " (a + b + c))
+function pedirValores() {
+  a = Number(prompt("Introduce valor de A"))
+  b = Number(prompt("Introduce valor de B"))
+  c = Number(prompt("Introduce valor de C"))
+}
+
+function imprime(nombre) {
+  document.getElementById("test").innerHTML =
+  "Este es el valor de a: " + a + "<br>Este es el valor de b: " + b + "<br>Este es el valor de c: " + c + "<br><br>Resultado de a + b + c = " + (a + b + c) + "<br><br>Mi nombre es " + nombre
+}
+
+// pedirValores()
+imprime("Pepe")
